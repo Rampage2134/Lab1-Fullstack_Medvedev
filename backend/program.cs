76a8c3f8235fd81dda6d1185f46a9b639233a977
Rampage2134 <medvedev201808@gmail.com>
2026-02-11 19:35:02 +0300

    Added input in program.cs

--- backend/program.cs
+++ backend/program.cs
@@ -1,3 +1,6 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Медведев Андрей Евгеньевич");
 Console.WriteLine(DateTime.Now);    
+
+string name = Console.ReadLine();
+Console.WriteLine($"Привет, {name}!");
